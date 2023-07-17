065d16e55ded166175f14cf1fc3b9ac1fddb70a3
Александр <mechanika.kap@gmail.com>
2023-07-17 08:47:46 +0300

    Домашнее Методы объектов

--- src/Main.java
+++ src/Main.java
@@ -3,31 +3,32 @@
 public class Main {
     public static void main(String[] args) {
         Author authorAlexanderDyuma = new Author("Alexander", "Dyuma");
-        Author authorLevTolstoy= new Author("Lev", "Tolstoy");
+        Author authorLevTolstoy = new Author("Lev", "Tolstoy");
         Book bookWarPeace = new Book("WarPeace", authorAlexanderDyuma, 1900);
         Book bookMorning = new Book("Morning", authorLevTolstoy, 1905);
         System.out.println("bookMorning.getBookYear = " + bookMorning.getBookYear());
         bookMorning.setBookYear(1925);
         System.out.println("bookMorning.getBookYear = " + bookMorning.getBookYear());
-    }
-    /* -------HomeworkObjectMethods below------- */
-    System.out.println("-------HomeworkObjectMethods below-------");
+
+        /* -------HomeworkObjectMethods below------- */
+        System.out.println("-------HomeworkObjectMethods below-------");
         Author authorAlexanderDyumaDuplicate = new Author("Alexander", "Dyuma");
         Book bookMorningDuplicate = new Book("Morning", authorLevTolstoy, 1905);
         System.out.println(bookWarPeace);
         System.out.println(bookMorning);
         System.out.println("(authorAlexanderDyuma.hashCode() == authorAlexanderDyumaDuplicate.hashCode()) :"
-            + (authorAlexanderDyuma.hashCode() == authorAlexanderDyumaDuplicate.hashCode()));
+                + (authorAlexanderDyuma.hashCode() == authorAlexanderDyumaDuplicate.hashCode()));
         System.out.println("(authorAlexanderDyuma.equals(authorAlexanderDyumaDuplicate) :"
-            + authorAlexanderDyuma.equals(authorAlexanderDyumaDuplicate));
+                + authorAlexanderDyuma.equals(authorAlexanderDyumaDuplicate));
         System.out.println("(authorAlexanderDyuma.hashCode() == authorLevTolstoy.hashCode()) :"
-            + (authorAlexanderDyuma.hashCode() == authorLevTolstoy.hashCode()));
+                + (authorAlexanderDyuma.hashCode() == authorLevTolstoy.hashCode()));
         System.out.println("(bookMorning.hashCode() == bookMorningDuplicate.hashCode()) :"
-            + (bookMorning.hashCode() == bookMorningDuplicate.hashCode()));
+                + (bookMorning.hashCode() == bookMorningDuplicate.hashCode()));
         System.out.println("(bookMorning.equals(bookMorningDuplicate)): "
-            + bookMorning.equals(bookMorningDuplicate));
+                + bookMorning.equals(bookMorningDuplicate));
         System.out.println("(bookMorning.hashCode() == bookWarPeace.hashCode()): "
-            + (bookMorning.hashCode() == bookWarPeace.hashCode()));
-        }
+                + (bookMorning.hashCode() == bookWarPeace.hashCode()));
+    }
+}
 
 
